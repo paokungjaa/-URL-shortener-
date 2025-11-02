0ed81e346303b067e54f5d051ababf98bf950424
paokungjaa <paokungjaa@gmail.com>
2025-11-02 20:24:28 +0700

    decoration the app thorgh CSS

---
+++ url-short/src/InputShortener.js
@@ -0,0 +1,14 @@
+
+
+const InputShortener = () => {
+  return (
+    <div className="inputcontainer">
+        <h1>URL<span> Shortener</span></h1>
+        <div><input type="text" placeholder="Paste a link here"/>
+        <button>Shorten</button>
+</div>
+    </div>
+  )
+}
+
+export default InputShortener
